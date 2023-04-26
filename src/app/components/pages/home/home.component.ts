2f3fb716142e4eb2dee95e3d4d8c7a978021a075
prashantrai27 <prashantrai317@gmail.com>
2023-04-26 19:06:44 +0530

    UI created with dummy data

---
+++ src/app/components/pages/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/food';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  foods:Food[] = [];
+
+  constructor(private foodService:FoodService){}
+
+  ngOnInit(): void {
+    this.foods = this.foodService.getAllData();
+    console.log(this.foods)
+  }
+}
